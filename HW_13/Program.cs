babccb9cb15925f6f102d467f247dcae0561595f
SergCherkasov <serg.cherkasov@gmail.com>
2023-05-10 14:47:30 +0300

    Add project files.

---
+++ HW_13/Program.cs
@@ -0,0 +1,20 @@
+﻿int x = 1;
+
+Mutex mutex = new();
+
+Thread thread1 = new(PrintNumbers);
+Thread thread2 = new(PrintNumbers);
+
+thread1.Start();
+thread2.Start();
+
+void PrintNumbers()
+{
+    for (; x < 100; x++)
+    {
+        mutex.WaitOne();
+        Console.WriteLine($"ThreadID: {Thread.CurrentThread.ManagedThreadId}: {x}");
+        Thread.Sleep(1000);
+        mutex.ReleaseMutex();
+    }
+}
